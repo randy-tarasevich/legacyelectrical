a440cf334cfafea18d929d73048f2c4988c3862b
Randy Tarasevich <95719739+randy-tarasevich@users.noreply.github.com>
2025-04-11 12:54:26 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,30 +1,40 @@
 loadNavBar()
 loadHome()
 loadAbout()
+loadFooter()
 
 function loadNavBar() {
-    const navbar = document.getElementById("navbar")
-    fetch("/components/navbar.html")
-        .then((response) => response.text())
-        .then((html) => {
-            navbar.innerHTML = html
-        })
+  const navbar = document.getElementById('navbar')
+  fetch('/components/navbar.html')
+    .then((response) => response.text())
+    .then((html) => {
+      navbar.innerHTML = html
+    })
 }
 
 function loadHome() {
-    const content = document.getElementById("content")
-    fetch("/components/home.html")
-        .then((response) => response.text())
-        .then((html) => {
-            content.innerHTML = html
-        })
+  const content = document.getElementById('content')
+  fetch('/components/home.html')
+    .then((response) => response.text())
+    .then((html) => {
+      content.innerHTML = html
+    })
 }
 
 function loadAbout() {
-    const content = document.getElementById("about")
-    fetch("/components/about.html")
-        .then((response) => response.text())
-        .then((html) => {
-            content.innerHTML = html
-        })
+  const content = document.getElementById('about')
+  fetch('/components/about.html')
+    .then((response) => response.text())
+    .then((html) => {
+      about.innerHTML = html
+    })
+}
+
+function loadFooter() {
+  const footer = document.getElementById('footer')
+  fetch('/components/footer.html')
+    .then((response) => response.text())
+    .then((html) => {
+      footer.innerHTML = html
+    })
 }
